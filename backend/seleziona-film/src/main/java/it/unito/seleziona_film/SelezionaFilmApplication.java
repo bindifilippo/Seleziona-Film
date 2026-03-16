d20b1eb7253df3f7b6e276b25ee9afecee6e9bd6
Filippo Bindi <192812197+bindifilippo@users.noreply.github.com>
2026-03-16 17:37:17 +0100

    inizializzazione spring

---
+++ backend/seleziona-film/src/main/java/it/unito/seleziona_film/SelezionaFilmApplication.java
@@ -0,0 +1,13 @@
+package it.unito.seleziona_film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelezionaFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelezionaFilmApplication.class, args);
+	}
+
+}
---
+++ backend/seleziona-film/src/test/java/it/unito/seleziona_film/SelezionaFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unito.seleziona_film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelezionaFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
