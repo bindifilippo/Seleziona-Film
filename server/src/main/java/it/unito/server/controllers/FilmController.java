d74d7e0cc6d4658137fb8145902c87b8f03445af
Filippo Bindi <192812197+bindifilippo@users.noreply.github.com>
2026-03-17 15:53:43 +0100

    controller film drammatici

--- server/src/main/java/it/unito/server/controllers/FilmController.java
+++ server/src/main/java/it/unito/server/controllers/FilmController.java
@@ -15,18 +15,21 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5500")
 public class FilmController {
 
-    @GetMapping("/films")
-    public List<Film> getFilms() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        ClassPathResource resource = new ClassPathResource("catalogo.json");
-        Catalogo catalogo = mapper.readValue(resource.getInputStream(), Catalogo.class);
-        return catalogo.getFilms();
-    }
-
     @GetMapping("/catalogo")
     public Catalogo getCatalogo() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         ClassPathResource resource = new ClassPathResource("catalogo.json");
         return mapper.readValue(resource.getInputStream(), Catalogo.class);
+    }
+
+     @GetMapping("/films/drammatici")
+    public List<Film> getFilmDrammatici() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ClassPathResource resource = new ClassPathResource("catalogo.json");
+        Catalogo catalogo = mapper.readValue(resource.getInputStream(), Catalogo.class);
+
+        return catalogo.getFilms().stream()
+                .filter(film -> film.getGenreIds() != null && film.getGenreIds().contains("Drammatico"))
+                .toList();
     }
 }
