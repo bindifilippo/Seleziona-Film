20a6e2f285818f3e2aaf2a8e4f95c99a6c8c0ae5
Filippo Bindi <192812197+bindifilippo@users.noreply.github.com>
2026-03-17 16:23:21 +0100

    ottimizzazione test

--- server/src/main/java/it/unito/server/controllers/FilmController.java
+++ server/src/main/java/it/unito/server/controllers/FilmController.java
@@ -12,14 +12,22 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5500")
+@CrossOrigin(origins = {"http://localhost:5500", "http://127.0.0.1:5500"})
 public class FilmController {
 
     @GetMapping("/catalogo")
     public Catalogo getCatalogo() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        ClassPathResource resource = new ClassPathResource("catalogo.json");
-        return mapper.readValue(resource.getInputStream(), Catalogo.class);
+    ObjectMapper mapper = new ObjectMapper();
+    ClassPathResource resource = new ClassPathResource("catalogo.json");
+    return mapper.readValue(resource.getInputStream(), Catalogo.class);
+    }
+
+     @GetMapping("/films")
+    public List<Film> getFilms() throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    ClassPathResource resource = new ClassPathResource("catalogo.json");
+    Catalogo catalogo = mapper.readValue(resource.getInputStream(), Catalogo.class);
+    return catalogo.getFilms();
     }
 
      @GetMapping("/films/drammatici")
